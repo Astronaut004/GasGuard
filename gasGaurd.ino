d3639a0096f9d0da8686f4b9b8f9621451ecc5aa
Astronaut004 <ac4548817@gmail.com>
2024-11-20 16:37:26 +0530

    data storage API for ML Model

--- gasGaurd.ino
+++ gasGaurd.ino
@@ -4,3 +4,7 @@
 // WiFi Credentials
 const char* ssid = "project";
 const char* password = "project007";
+
+// ThingSpeak API
+const char* server = "http://api.thingspeak.com/update";
+String apiKey = "H8WP6AJFT410LCQ9";
