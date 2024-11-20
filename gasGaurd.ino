0955028bb1bc3bd6ab35376aae7376b6fc51056b
Astronaut004 <ac4548817@gmail.com>
2024-11-20 23:00:12 +0530

    setup for continuos checking

--- gasGaurd.ino
+++ gasGaurd.ino
@@ -54,3 +54,91 @@ void setup() {
   pinMode(buzzerPin, OUTPUT);
   digitalWrite(buzzerPin, LOW);
 }
+
+void loop() {
+  int mq2Value = analogRead(mq2Pin);
+  int mq4Value = analogRead(mq4Pin);
+  int mq9Value = analogRead(mq9Pin);
+
+  // Calculate Gas Concentrations
+  float mq2Concentration[5];
+  calculateMQ2(mq2Value, mq2Concentration);
+
+  float mq4Concentration[2];
+  calculateMQ4(mq4Value, mq4Concentration);
+
+  float mq9Concentration[2];
+  calculateMQ9(mq9Value, mq9Concentration);
+
+  Serial.println("MQ2 Concentrations:");
+  Serial.print("LPG: "); Serial.println(mq2Concentration[0]);
+  Serial.print("Propane: "); Serial.println(mq2Concentration[1]);
+  Serial.print("Hydrogen: "); Serial.println(mq2Concentration[2]);
+  Serial.print("Smoke: "); Serial.println(mq2Concentration[3]);
+  Serial.print("Methane (MQ2): "); Serial.println(mq2Concentration[4]);
+
+  Serial.println("MQ4 Concentrations:");
+  Serial.print("Methane (MQ4): "); Serial.println(mq4Concentration[0]);
+  Serial.print("CNG: "); Serial.println(mq4Concentration[1]);
+
+  Serial.println("MQ9 Concentrations:");
+  Serial.print("CO: "); Serial.println(mq9Concentration[0]);
+  Serial.print("Methane (MQ9): "); Serial.println(mq9Concentration[1]);
+
+
+  // Compose SMS Message
+  String alertMessage = "";
+  bool alert = false;
+
+  if (checkAndAlert(mq2Value, mq2Threshold, mq2LED, "MQ2")) {
+    alert = true;
+    alertMessage += "MQ2 Alert (LPG, Propane, Hydrogen, Smoke, Methane)\n";
+  }
+  if (checkAndAlert(mq4Value, mq4Threshold, mq4LED, "MQ4")) {
+    alert = true;
+    alertMessage += "MQ4 Alert (Methane, CNG)\n";
+  }
+  if (checkAndAlert(mq9Value, mq9Threshold, mq9LED, "MQ9")) {
+    alert = true;
+    alertMessage += "MQ9 Alert (CO, Methane)\n";
+  }
+
+  if (alert) {
+    digitalWrite(buzzerPin, HIGH);
+    sendSMS(alertMessage);
+    delay(2000); // Buzzer duration
+    digitalWrite(buzzerPin, LOW);
+  }
+
+  // Upload Data to ThingSpeak
+  if (WiFi.status() == WL_CONNECTED) 
+  {
+    HTTPClient http;
+    String url = String(server) + "?api_key=" + apiKey +
+                 "&field1=" + String(mq2Concentration[0]) + // LPG
+                 "&field2=" + String(mq2Concentration[1]) + // Propane
+                 "&field3=" + String(mq2Concentration[2]) + // Hydrogen
+                 "&field4=" + String(mq2Concentration[3]) + // Smoke
+                 "&field5=" + String(mq2Concentration[4]) + // Methane (MQ2)
+                 "&field6=" + String(mq4Concentration[0]) + // Methane (MQ4)
+                 "&field7=" + String(mq4Concentration[1]) + // CNG
+                 "&field8=" + String(mq9Concentration[0]);  // CO
+    http.begin(url);
+    int httpCode = http.GET();
+    if (httpCode > 0) 
+    {
+      Serial.println("Data sent to ThingSpeak");
+    } 
+    else 
+    {
+      Serial.println("Error in ThingSpeak communication");
+    }
+    http.end();
+  } 
+  else 
+  {
+    Serial.println("WiFi Disconnected");
+  }
+
+  delay(5000); // Adjust for ThingSpeak upload rate
+}
